839a271ba8237df0ce46c7bfc1c6d6406142f0cb
kvammee <elliott.kvamme@gmail.com>
2022-01-21 13:31:23 -0600

    Debugged and ran db and schemas

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS emp_role;
+DROP TABLE IF EXISTS department;
 
-CREATE TABLE department(
+CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
-CREATE TABLE role(
+CREATE TABLE emp_role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER,
+    department_id INTEGER NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-)
+);
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER
-    manager_id INTEGER
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-)
+    emp_role_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    CONSTRAINT fk_emp_role FOREIGN KEY (emp_role_id) REFERENCES emp_role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
