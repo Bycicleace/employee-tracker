96d4f3e16f96fd0ce1a1e78950db42cf0caeec44
kvammee <elliott.kvamme@gmail.com>
2022-01-21 14:55:32 -0600

    Fixed seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,52 +2,58 @@ INSERT INTO department (name)
 VALUES
     ('Executives'),
     ('Application Development'),
-    ('Infrastructure Management')
+    ('Infrastructure Management'),
     ('Operations Management'),
     ('Cyber Security'),
     ('User Support & Service');
 
 INSERT INTO emp_role (title, salary, department_id)
 VALUES
-    ('CTO', 200000, 0),
-    ('Development Manager', 120000, 1)
-    ('Software Developer', 95000, 1),
-    ('Data Science Technician', 90000, 1),
-    ('Infrastructure Manager', 100000, 2),
-    ('Desktop Technician', 48000, 2),
-    ('Network Technician', 57000, 2),
-    ('Network Administrator', 82000, 2),
-    ('Operations Manager', 100000, 3),
-    ('Application Administrator', 80000, 3),
-    ('Quality Assurance Analyst', 60000, 3),
-    ('Security Manager', 120000, 4),
-    ('Security Technician', 90000, 4),
-    ('Customer Service Manager', 80000, 5),
-    ('Customer Service Representative', 50000, 5);
+    ('CTO', 200000.00, 1),
+    ('Development Manager', 120000.00, 2),
+    ('Software Developer', 95000.00, 2),
+    ('Data Science Technician', 90000.00, 2),
+    ('Infrastructure Manager', 100000.00, 3),
+    ('Desktop Technician', 48000.00, 3),
+    ('Network Technician', 57000.00, 3),
+    ('Network Administrator', 83000.00, 3),
+    ('Operations Manager', 100000.00, 4),
+    ('Application Administrator', 80000.00, 4),
+    ('Quality Assurance Analyst', 60000.00, 4),
+    ('Security Manager', 120000.00, 5),
+    ('Security Technician', 90000.00, 5),
+    ('Customer Service Manager', 80000.00, 6),
+    ('Customer Service Rep', 50000.00, 6);
 
 INSERT INTO employee (first_name, last_name, emp_role_id, manager_id)
 VALUES
-    ('Mabel', 'Walter', 0, NULL),
-    ('Alexander', 'Hand', 1, 0),
-    ('Gracie', 'Kerluke', 4, 0),
-    ('Nicholas Mueller', 8, 0),
-    ('Americo', 'Kilback', 11, 0),
-    ('Marcos', 'Lemke', 13, 0),
-    ('Audreanne', 'Gusikowski', 2, 1),
-    ('Santos', 'Emmerich', 2, 1),
-    ('Sierra', 'Bergstrom', 3, 1),
-    ('Ova', 'Barton', 3, 1),
-    ('Jerry', 'Bergnaum', 5, 4),
-    ('Cicero', 'Von', 5, 4),
-    ('Madyson', 'Fisher', 6, 4),
-    ('Johnny', 'Simmons', 6, 4),
-    ('Lizeth', 'Rempel', 7, 4),
-    ('Thelma', 'Heller', 9, 8),
-    ('Isaac', 'McLaughlin', 10, 8),
-    ('Katlyn', 'Little', 10, 8),
-    ('Kaelyn', 'Schultz', 12, 11),
-    ('Cameron', 'Labadie', 14, 13),
-    ('Anissa', 'Lueilwitz', 14, 13),
-    ('Abigail', 'Kuphal', 14, 13),
-    ('Autumn', 'Grady', 14, 13),
-    ('Enos', 'Sawayn', 14, 13);
+    ('Mabel', 'Walter', 1, NULL);
+
+INSERT INTO employee (first_name, last_name, emp_role_id, manager_id)
+VALUES
+    ('Alexander', 'Hand', 2, 1),
+    ('Gracie', 'Kerluke', 5, 1),
+    ('Nicholas', 'Mueller', 9, 1),
+    ('Americo', 'Kilback', 12, 1),
+    ('Marcos', 'Lemke', 14, 1);
+
+INSERT INTO employee (first_name, last_name, emp_role_id, manager_id)
+VALUES
+    ('Audreanne', 'Gusikowski', 3, 2),
+    ('Santos', 'Emmerich', 3, 2),
+    ('Sierra', 'Bergstrom', 4, 2),
+    ('Ova', 'Barton', 4, 2),
+    ('Jerry', 'Bergnaum', 6, 5),
+    ('Cicero', 'Von', 6, 5),
+    ('Madyson', 'Fisher', 7, 5),
+    ('Johnny', 'Simmons', 7, 5),
+    ('Lizeth', 'Rempel', 8, 5),
+    ('Thelma', 'Heller', 10, 9),
+    ('Isaac', 'McLaughlin', 11, 9),
+    ('Katlyn', 'Little', 11, 9),
+    ('Kaelyn', 'Schultz', 13, 12),
+    ('Cameron', 'Labadie', 15, 14),
+    ('Anissa', 'Lueilwitz', 15, 14),
+    ('Abigail', 'Kuphal', 15, 14),
+    ('Autumn', 'Grady', 15, 14),
+    ('Enos', 'Sawayn', 15, 14);
